d45ceb57d5583fd4c8efac81c7cfabdd9c389b82
Nicolas Caillouet <nicolascaillouet@pc95.home>
2020-03-29 12:15:56 +0200

    1er commit pour mettre le repo sur github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import BoutonResa from './components/BoutonResa'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiResponse:'',
+      email:''
+    }
+  }
+
+  callApi() {
+  fetch('http://localhost:9000/testApi')
+    .then(res => res.text())
+    .then(res => this.setState({apiResponse: res}))
+  }
+
+  componentWillMount() {
+    this.callApi()
+  }
+
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('prout')
+  }
+
+  handleChange = (event) => {
+    const email = event.target.value
+    this.setState({ email })
+  }
+
+  render() {
+    return (
+      <div className='box'>
+        <div>
+          <h2>Réservez un créneau avec Nicolas de Smile</h2>
+        </div>
+        <div >
+          <BoutonResa></BoutonResa>
+        </div>
+        <div >
+          <form
+            className='formResa'
+            onSubmit={this.handleSubmit}>
+            <input
+              type="email"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+              placeholder='votre adresse mail'
+              className='inputResa'
+              onChange={this.handleChange}/>
+            <button type='submit' className='boutonResa'>Créer rendez-vous</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/BoutonResa.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class BoutonResa extends Component {
+  render() {
+    return(
+      <div className='areaBouton'>
+        <button className='eventButton'>Test</button>
+      </div>
+    )
+  }
+}
+
+export default BoutonResa
