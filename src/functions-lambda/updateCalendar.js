7739cf8ea60bcfa312b3855193e6da9f8f2ff37c
Nicolas Caillouet <nicolas.caillouet@gmail.com>
2020-04-08 11:37:35 +0200

    changé nom foncton lambda netlify

--- src/functions-lambda/updateCalendar.js
+++ src/functions-lambda/updateCalendar.js
@@ -1,8 +1,20 @@
+const { google } = require("googleapis")
+const {JWT} = require('google-auth-library');
+
+require('dotenv').config()
 
 exports.handler = async (event, context) => {
-  const faireProut = function prout() {
-    return prout = 'test'
-  }
+
+  const upCal = await calendar.events.update({
+      calendarId: process.env.CALENDAR_ID,
+      eventId : varà récupérer,
+      timeMin: (new Date()).toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime',
+    });
+
+
   return {
     statusCode: 200,
     body : JSON.stringify({res : faireProut()})
