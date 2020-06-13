b7f3189ec49eb0a59423f66dcc8094e5bfea6a3b
Geoffroy Mounier <geoffroymounier@gmail.com>
2020-06-13 20:28:13 +0200

    add attendees script

--- src/functions-lambda/updateCalendar.js
+++ src/functions-lambda/updateCalendar.js
@@ -3,20 +3,59 @@ const {JWT} = require('google-auth-library');
 
 require('dotenv').config()
 
-exports.handler = async (event, context) => {
+const newClient = new google.auth.JWT(
+  process.env.CLIENT_EMAIL,
+  null,
+  process.env.PRIVATE_KEY,
+  ['https://www.googleapis.com/auth/calendar'],
+  "contact@mkmstudio.fr"
+);
 
-  const upCal = await calendar.events.update({
-      calendarId: process.env.CALENDAR_ID,
-      eventId : varà récupérer,
-      timeMin: (new Date()).toISOString(),
-      maxResults: 10,
-      singleEvents: true,
-      orderBy: 'startTime',
-    });
+const calendar = google.calendar({
+    version: 'v3',
+    project: process.env.PROJECT_NUMBER,
+    auth: newClient
+  });
+
+exports.handler = async (event, context) => {
+  const { email, card } = JSON.parse(event.body)
+
+  try {
+    const upCal = await calendar.events.update({
+        calendarId: process.env.CALENDAR_ID,
+        eventId : card.id,
+        resource : {
+          end : card.end,
+          start : card.start,
+          attendees : [...(card.attendees||[]),{email}]
+        }
+
+        // maxResults: 10,
+        // singleEvents: true,
+        // orderBy: 'startTime',
+      });
+    console.log(upCal)
+  } catch (e) {
+    console.log(e)
+  }
 
 
   return {
     statusCode: 200,
-    body : JSON.stringify({res : faireProut()})
+    body : JSON.stringify({res : ''})
   }
+  // const upCal = await calendar.events.update({
+  //     calendarId: process.env.CALENDAR_ID,
+  //     eventId : varà récupérer,
+  //     timeMin: (new Date()).toISOString(),
+  //     maxResults: 10,
+  //     singleEvents: true,
+  //     orderBy: 'startTime',
+  //   });
+  //
+  //
+  // return {
+  //   statusCode: 200,
+  //   body : JSON.stringify({res : faireProut()})
+  // }
 }
